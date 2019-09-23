593031d32b8408d594001cde743b9e1b1d2dfad4
ILoveMilktea <43364424+ILoveMilktea@users.noreply.github.com>
2019-09-23 13:48:11 +0900

    temp player script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoSingleton<Player>, IPlayerMove
+{
+    private PlayerMove playerMove = new PlayerMove();
+    public float moveSpeed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void Move(Vector3 dir, float amount)
+    {
+        //Vector3 targetPos = transform.position + dir * amount * moveSpeed;
+        //transform.position = Vector3.Slerp(transform.position, targetPos, Time.deltaTime);
+
+        transform.position = transform.position + dir * amount * moveSpeed * Time.deltaTime;
+    }
+
+    public void Stop()
+    {
+        transform.position = transform.position;
+    }
+}
